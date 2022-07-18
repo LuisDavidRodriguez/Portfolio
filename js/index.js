483c046373c679376bc78e397bb86fc864a7d71b
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-18 15:46:11 -0500

    Fix linters errors, and ignore the sass file

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-const body = document.body;
-const modal = document.querySelector("#menuModal");
-const burger = document.querySelector("#navBurger");
-const closeButton = document.querySelector("#closeButton");
-const menuOptions = document.querySelector("#menuList");
+const modal = document.querySelector('#menuModal');
+const burger = document.querySelector('#navBurger');
+const closeButton = document.querySelector('#closeButton');
+const menuOptions = document.querySelector('#menuList');
 
-burger.addEventListener('click', ()=>{
-    modal.classList.add('open-menu');
-    modal.classList.remove('close-menu');
-    modal.style.display = "block";
+burger.addEventListener('click', () => {
+  modal.classList.add('open-menu');
+  modal.classList.remove('close-menu');
+  modal.style.display = 'block';
 });
 
-closeButton.addEventListener('click', ()=>{
-    modal.classList.add('close-menu');
-    modal.classList.remove('open-menu');
-    setTimeout(()=>{
-        modal.style.display = "none";
-    }, 500);
+closeButton.addEventListener('click', () => {
+  modal.classList.add('close-menu');
+  modal.classList.remove('open-menu');
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 500);
 });
 
-menuOptions.addEventListener('click', ()=>{
-    modal.style.display = "none";
+menuOptions.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
