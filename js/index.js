56c3222813e688f040fa22dacf4bdbef5e2ecb30
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-07-18 17:20:27 -0300

    Add animation clases, add transitions to menu

--- js/index.js
+++ js/index.js
@@ -5,11 +5,17 @@ const closeButton = document.querySelector("#closeButton");
 const menuOptions = document.querySelector("#menuList");
 
 burger.addEventListener('click', ()=>{
+    modal.classList.add('open-menu');
+    modal.classList.remove('close-menu');
     modal.style.display = "block";
 });
 
 closeButton.addEventListener('click', ()=>{
-    modal.style.display = "none";
+    modal.classList.add('close-menu');
+    modal.classList.remove('open-menu');
+    setTimeout(()=>{
+        modal.style.display = "none";
+    }, 500);
 })
 
 menuOptions.addEventListener('click', ()=>{
