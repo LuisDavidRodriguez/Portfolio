57a38b5b1bf13590f348abd93c4d0d865b74c869
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-18 12:05:36 -0500

    Add modal and js file

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const body = document.body;
+const modal = document.querySelector("#menuModal");
+const burger = document.querySelector("#navBurger");
+
+burger.addEventListener('click', ()=>{
+    modal.style.display = "block";
+});
