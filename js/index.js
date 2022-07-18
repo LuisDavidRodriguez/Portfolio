6a4ef4bd073f9a67314227a7de985d4a99903bad
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-07-18 14:44:17 -0300

    Add list and cross button to menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 const body = document.body;
 const modal = document.querySelector("#menuModal");
 const burger = document.querySelector("#navBurger");
+const closeButton = document.querySelector("#closeButton");
+const menuOptions = document.querySelector("#menuList");
 
 burger.addEventListener('click', ()=>{
     modal.style.display = "block";
 });
+
+closeButton.addEventListener('click', ()=>{
+    modal.style.display = "none";
+})
+
+menuOptions.addEventListener('click', ()=>{
+    modal.style.display = "none";
+})
