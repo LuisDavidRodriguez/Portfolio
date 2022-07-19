11089118658c075656963b6a1bb61e38aa3911d8
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-07-19 17:41:26 -0300

    Add const to js file

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@ const modal = document.querySelector('#menuModal');
 const burger = document.querySelector('#navBurger');
 const closeButton = document.querySelector('#closeButton');
 const menuOptions = document.querySelector('#menuList');
+const workButton = document.querySelector('#workSeeBtn');
+const projectPup = document.querySelector('#popUp');
+const projectButton1 = document.querySelector('#projectBtn1');
+const pupClose = document.querySelector('#popUpCloseButton');
+const pTitle = document.querySelector('#popUpTitle');
+const pTech1 = document.querySelector('#popUpTech1');
+const pTech2 = document.querySelector('#popUpTech2');
+const pTech3 = document.querySelector('#popUpTech3');
+const pTech4 = document.querySelector('#popUpTech4');
+const pImg = document.querySelector('#popUpImg');
+const pTxt = document.querySelector('#popUpDesc');
+const pBtnLive = document.querySelector('#popUpLive');
+const pBtnSource = document.querySelector('#popUpSource');
 
 burger.addEventListener('click', () => {
   modal.classList.add('open-menu');
